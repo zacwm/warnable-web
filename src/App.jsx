fa0ee8d1179a132d29607d36d084da07b1cf0e1b
Zachary <me@zachary.fun>
2021-08-08 15:29:44 +1000

    route test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Editor from './Routes/Editor';
 import ErrorPage from './Routes/ErrorPage';
 import HomePage from './Routes/Home';
@@ -7,15 +7,13 @@ export default function App() {
   return (
     <>
       <Router basename="/">
-        <Switch>
-          { /* # Editor */}
-          <Route path="/editor/NotFound" exact> <ErrorPage type="editorCode" /> </Route>
-          <Route path="/editor/:code" exact children={<Editor />} />
-          { /* # Home */ }
-          <Route path="/" exact> <HomePage /> </Route>
-          { /* # 404 */ }
-          <Route path="/*" exact> <ErrorPage type="noPage" /> </Route>
-        </Switch>
+        { /* # Editor */}
+        <Route path="/editor/NotFound" exact> <ErrorPage type="editorCode" /> </Route>
+        <Route path="/editor/:code" exact children={<Editor />} />
+        { /* # Home */ }
+        <Route path="/" exact> <HomePage /> </Route>
+        { /* # 404 */ }
+        <Route path="/*" exact> <ErrorPage type="noPage" /> </Route>
       </Router>
     </>
   );
