21853a8a1f365a3d99d01c3c7a724ef4556ada03
Zachary <me@zachary.fun>
2021-08-08 13:35:46 +1000

    action test

--- src/Routes/Home/index.jsx
+++ src/Routes/Home/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home({ type }) {
   return (
     <div className="HomeParent">
-      <p>Hey!:) GIT_COMMIT_HASH</p>
+      <p>Hey!:)</p>
     </div>
   );
 };
