5ac52053a7439cd8b243f3c8d83914d62c876721
Zachary <me@zachary.fun>
2021-08-26 21:16:13 +1000

    Finish punishments

--- src/Routes/Editor/Punishments.jsx
+++ src/Routes/Editor/Punishments.jsx
@@ -3,6 +3,18 @@ import Select from "react-select";
 import Twemoji from "../../Components/Twemoji";
 
 export default function EditorPage({ SessionData, EditData, setEdit }) {
+  function updateData(i, k, v) {
+    let tempEdit = EditData;
+    let tempPunishment = tempEdit.punishments;
+    let tempIndex = tempPunishment[i];
+    tempIndex[k] = v;
+    tempPunishment[i] = tempIndex;
+    setEdit({
+      ...tempEdit,
+      punishments: [...tempPunishment]
+    });
+  }
+
   return (
     <div className="EditorPageParent">
       <p className="mainText">Punishments</p>
@@ -14,16 +26,20 @@ export default function EditorPage({ SessionData, EditData, setEdit }) {
         <div className="punishList">
           {
             EditData.punishments?.map((v, i) => (
-              <div className="punishItem">
+              <div className="punishItem" key={i}>
                 <div className="sub itemIndex">
                   <span>{i + 1}.</span>
                 </div>
                 <div className="main">
-                  <p><b>Range:</b> <input type="text" defaultValue={v.rangeMin}></input> to <input type="text" defaultValue={v.rangeMax}></input></p>
+                  <p><b>Range:</b> <input type="text" value={v.rangeMin} onChange={(nv) => {
+                    updateData(i, 'rangeMin', nv.target.value);
+                  }}></input> to <input type="text" value={v.rangeMax} onChange={(nv) => {
+                    updateData(i, 'rangeMax', nv.target.value);
+                  }}></input></p>
                   <span><b>Action:</b> </span>
                   <Select
                     className="selectItem"
-                    defaultValue={{ 
+                    value={{ 
                       mute: { value: 'mute', label: 'Mute' },
                       kick: { value: 'kick', label: 'Kick' },
                       ban: { value: 'ban', label: 'Ban' }
@@ -33,25 +49,42 @@ export default function EditorPage({ SessionData, EditData, setEdit }) {
                       { value: 'kick', label: 'Kick' },
                       { value: 'ban', label: 'Ban' }
                     ]}
+                    onChange={(nv) => {
+                      updateData(i, 'actionType', nv.value);
+                    }}
                   />
-                  <p className="smt"><b>Temporary time</b> (Leave blank for permanent): <input type="text" defaultValue={v.tempTime}></input></p>
+                  <p className="smt"><b>Temporary time</b> (Leave blank for permanent): <input type="text" value={v.tempTime} onChange={(nv) => {
+                    updateData(i, 'tempTime', nv.target.value);
+                  }}></input></p>
                 </div>
                 <div className="sub">
-                  <span className="btnDeleteItem"><Twemoji emoji="🗑️" /></span>
+                  <span className="btnDeleteItem" onClick={() => {
+                    let punishmentItems = EditData.punishments;
+                    punishmentItems.splice(i, 1)
+                    setEdit(prevState => {
+                      return {
+                        ...prevState, 
+                        punishments: [...punishmentItems]
+                      } 
+                    });
+                  }}><Twemoji emoji="🗑️" /></span>
                 </div>
               </div>
             ))
           }
-          <div className="createItem" onClick={() => {
-            let tempEdit = EditData;
-            tempEdit.punishments.push({
-              rangeMin: '',
-              rangeMax: '',
-              actionType: '',
-              tempTime: ''
-            })
-            setEdit(tempEdit);
-          }}>
+          <div className="createItem" onClick={(() => {
+            setEdit(prevState => {
+              return {
+                ...prevState, 
+                punishments: [...prevState.punishments, {
+                  rangeMin: '',
+                  rangeMax: '',
+                  actionType: '',
+                  tempTime: ''
+                }]
+              } 
+            });
+          })}>
             <p>Create new punishment</p>
           </div>
         </div>
